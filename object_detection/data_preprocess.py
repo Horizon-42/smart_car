ffb41bd03c965be25dacf45691748d4b8dd3e084
ldx <ldx@xiaofutech.com>
2026-02-07 02:18:20 +0100

    fix: update dataset directories for image preprocessing

--- object_detection/data_preprocess.py
+++ object_detection/data_preprocess.py
@@ -3,13 +3,13 @@ import shutil
 import cv2
 from tqdm import tqdm
 
-dir1 = "object_detection/data/dataset_object_detection_640X480/20260206_091206/images"
-dir2 = "object_detection/data/dataset_object_detection_640X480/20260206_091727/images"
+dir1 = "object_detection/data/dataset_object_detection/20260206_060944/images"
+dir2 = "object_detection/data/dataset_object_detection/20260206_061544/images"
 
 ims1 = [os.path.join(dir1, f) for f in os.listdir(dir1) if f.endswith('.jpg')]
 ims2 = [os.path.join(dir2, f) for f in os.listdir(dir2) if f.endswith('.jpg')]
 
-dataset_dir = "object_detection/data/combined_dataset_640X480/images"
+dataset_dir = "object_detection/data/combined_dataset/images"
 
 if not os.path.exists(dataset_dir):
     os.makedirs(dataset_dir)
